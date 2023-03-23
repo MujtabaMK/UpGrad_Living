a69dae81dea5030d5b37719d3cafe67b98a002bc
MujtabaKhan <mujtabakhanakola@gmail.com>
2023-03-23 11:08:15 +0530

    Initial Commit

---
+++ Upgrad Living/Upgrad_LivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Upgrad_LivingApp.swift
+//  Upgrad Living
+//
+//  Created by Mujtaba Khan on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Upgrad_LivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
